e7a4965decb7f22d6ae5040e0be04bf727b19ebd
Billy J. Beltran <billy@redpill-linpro.com>
2022-04-07 15:30:07 +0200

    Add spectral_data key

--- src/Graph.js
+++ src/Graph.js
@@ -6,10 +6,12 @@ import {
   LinearScale,
   PointElement,
   LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 import { Line } from 'react-chartjs-2';
 
 
@@ -17,6 +19,7 @@ ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
+  BarElement,
   LineElement,
   Title,
   Tooltip,
@@ -32,7 +35,8 @@ const dataset_labels = {
   "soil_temp":"Soil Temperature",
   "ambient_temp": "Ambient Temperature",
   "moisture_level": "Moisture Level",
-  "light_measurement": "Light Measurement"
+  "light_measurement": "Light Measurement",
+  "spectral_data" : "Spectral Measurement"
 };
 
 
@@ -61,6 +65,22 @@ class Graph extends React.Component {
     render(){
         const { DataisLoaded, items } = this.state;
         const labels = items.map(msmt => msmt.date_time);
+	const spectral_options = {
+	    scales: {
+		y: {
+		    beginAtZero: true
+		}},
+            responsive: true,
+            plugins: {
+		legend: {
+              position: 'top',
+		},
+		title: {
+		    display: true,
+              text: 'Line Chart for ' + this.props.device_id,
+		},
+            },
+        };
         const options = {
           responsive: true,
           plugins: {
@@ -106,6 +126,37 @@ class Graph extends React.Component {
               },
             ],
           };
+	const spectral_labels = ["violet 415nm","blue 445nm","blue 480nm", "green 515nm",
+				 "green 555nm", "green 590nm", "red 630nm", "red 680nm", "near ir 910 nm", "clear 350-1000 nm"];	
+	const spectral_data = {
+	    labels: spectral_labels,
+	    datasets: [{
+		label: 'Spectral measurement',
+		data: items.at(-1).spectral_data,
+		backgroundColor: [
+		    'rgba(118, 0, 237, 1)',
+		    'rgba(0,40,255, 1)',
+		    'rgba(0,213,255,1)',
+		    'rgba(31,255,0,1)',
+		    'rgba(179, 255, 0, 1)',
+		    'rgba(255, 223, 0, 1)',
+		    'rgba(255, 79, 0, 1)',
+		    'rgba(255,0,0,1)',
+		    'rgba(97,0,0,1)',
+		    'rgba(0,255,181,0.3)'
+		],
+		borderColor: [
+		    'rgb(255, 99, 132)',
+		    'rgb(255, 159, 64)',
+		    'rgb(255, 205, 86)',
+		    'rgb(75, 192, 192)',
+		    'rgb(54, 162, 235)',
+		    'rgb(153, 102, 255)',
+		    'rgb(201, 203, 207)'
+		],
+		borderWidth: 0
+	    }]
+	};
           const data_light = {
             labels,
             datasets: [
@@ -164,6 +215,9 @@ class Graph extends React.Component {
                 <div className="chart-container">
                   <Line options={options} data={data_light} />
                 </div>
+		<div className="chart-container">
+		  <Bar options={spectral_options} data={spectral_data} />
+		</div>
               </div>
             </div>
         );
