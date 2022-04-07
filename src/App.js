7e313c0dac061ae9f462360d365e69f7bc413406
Alexander Gomez <alexander@redpill-linpro.com>
2022-04-07 15:08:19 +0200

    Change header

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Plantmonitor</h2>
         </div>
         <div className="App-intro">
           <GraphList></GraphList>
